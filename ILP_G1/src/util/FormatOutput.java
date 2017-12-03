d210b5156555636fba17df9310a54c08537ff7f0
imjuoy <bsubhadeep95@gmail.com>
2017-12-02 20:13:08 -0500

    Uncommented two sysouts

--- ILP_G1/src/util/FormatOutput.java
+++ ILP_G1/src/util/FormatOutput.java
@@ -18,8 +18,8 @@ public class FormatOutput {
 			c = upKEY.charAt(i);
 			pos[i]=upTXT.indexOf(c);
 		}
-		//System.out.println(pos[0]);
-		//System.out.println(pos[1]);
+		System.out.println(pos[0]);
+		System.out.println(pos[1]);
 		int h=0;
 		//System.out.println("h value outside loop="+h);
 	
